2a94c3f3e56b3b77440271d0ed5257fa15db33f5
MariiaVashchenko <maschunder@gmail.com>
2023-03-16 14:54:21 +0300

    second programm

--- hz.py
+++ hz.py
@@ -1,2 +1,4 @@
-name=input('hhhhhh?')
-print (fr'hhhhh {name}')
+s = 'In Python, numeric types are float and int'
+# довжина строки - 42 символи
+# перший символ має індекс 0, останній - 41
+print(s.rfind('f'))
